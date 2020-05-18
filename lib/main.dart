3fba5e038d8b84e85e6d86742064c5907de9c880
Your Name <ishokarobertt@gmail.com>
2020-05-18 15:43:40 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer = quizBrain.getCorrectAnswer();
+
+    setState(() {
+      //Use IF/ELSE to check if we've reached the end of the quiz. If true, execute Part A, B, C, D.
+      if (quizBrain.isFinished() == true) {
+        //show an alert using rFlutter_alert (remember to read the docs for the package!)
+
+        Alert(
+          context: context,
+          type: AlertType.warning,
+          title: "End of Quiz",
+          desc: "You Scored:""Do you want to play quiz again.",
+          buttons: [
+            DialogButton(
+              child: Text(
+                "YES",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              //reset the questionNumber,
+              onPressed: () => Navigator.pop(context, quizBrain.reset(),),
+              color: Color.fromRGBO(0, 179, 134, 1.0),
+            ),
+            DialogButton(
+              child: Text(
+                "Quit Quiz",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () => SystemNavigator.pop(),
+              gradient: LinearGradient(colors: [
+                Color.fromRGBO(116, 116, 191, 1.0),
+                Color.fromRGBO(52, 138, 199, 1.0)
+              ]),
+            )
+          ],
+        ).show();
+        //empty out the scoreKeeper.
+        scoreKeeper = [];
+        //If we've not reached the end, ELSE do the answer checking steps below 👇
+      } else {
+        if (userPickedAnswer == correctAnswer) {
+          scoreKeeper.add(Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+        } else {
+          scoreKeeper.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+        quizBrain.nextQuestion();
+      }
+    });
+  }
+
+  //linear progress indicator.
+
+  @override
+  void initState() {
+    super.initState();
+    quizBrain.progressValue = 0.0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          child: Image.asset(
+            'assets/images/random.png',
+          ),
+        ),
+        Container(
+          child: LinearProgressIndicator(
+            value: quizBrain.progressValue,
+            backgroundColor: Colors.cyanAccent,
+            valueColor: new AlwaysStoppedAnimation<Color>(Colors.orangeAccent),
+          ),
+        ),
+        Expanded(
+          flex: 4,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //The user picked true.
+                checkAnswer(true);
+                setState(() {
+                  quizBrain.progressValue += 1.0/24;
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+                checkAnswer(false);
+                setState(() {
+                  quizBrain.progressValue += 0.0666666666666;
+                });
+              },
+            ),
+          ),
+        ),
+        Wrap(
+            children: scoreKeeper,
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/quiz_brain.dart
@@ -0,0 +1,80 @@
+import 'question.dart';
+
+class QuizBrain {
+  int _questionNumber = 0;
+  double progressValue = 0;
+
+  List<Question> _questionBank = [
+    Question(
+        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        false),
+    Question('Google was originally called \"Backrub\".', true),
+    Question(
+        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        true),
+    Question('Abraham Lincoln had no middle name.', true),
+    Question('John Lennon\'s middle name was Edward.', false),
+    Question('Japan has square watermelons.', true),
+    Question(
+        'Pinocchio was the first animated, full-color Walt Disney feature film.',
+        false),
+    Question('Porcupines can float.', true),
+    Question('Before becoming queen, Queen Elizabeth was a mechanic.', true),
+    Question(
+        'In some states, it\'s illegal to eat specific foods while walking backwards.',
+        true),
+    Question('Cows sleep standing up', true),
+    Question('Donald Duck\'s sister is named Donna.', false),
+    Question('Twinkies originally had banana filling', true),
+    Question('People in Japan eat Kentucky Fried Chicken for Christmas dinner',
+        true),
+    Question('Fortune cookies were invented in China', false),
+    Question(
+        'It\'s illegal in Georgia to eat fried chicken with a knife and fork.',
+        true),
+    Question('Some cats are actually allergic to humans', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
+    Question('It is illegal to pee in the Ocean in Portugal.', true),
+    Question(
+        'No piece of square dry paper can be folded in half more than 7 times.',
+        false),
+    Question(
+        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        true),
+  ];
+
+  void nextQuestion() {
+    if (_questionNumber < _questionBank.length - 1) {
+      _questionNumber++;
+    }
+  }
+
+  String getQuestionText() {
+    return _questionBank[_questionNumber].questionText;
+  }
+
+  bool getCorrectAnswer() {
+    return _questionBank[_questionNumber].questionAnswer;
+  }
+
+  // method is finished
+  bool isFinished() {
+    if (_questionNumber >= _questionBank.length - 1) {
+      print('Now returning true');
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  //Use a print statement to check that isFinished is returning true when you are indeed at the end of the quiz and when a restart should happen.
+
+  //Create a reset() method here that sets the questionNumber back to 0.
+  void reset() {
+    _questionNumber = 0;
+    progressValue = 0;
+  }
+}
